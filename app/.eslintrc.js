e2006ee69632e573ecea227266e6e7bba04ec250
Jichan <joseph@zeronsoftn.com>
2020-06-23 17:11:36 +0900

    style: fix lint

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -28,4 +28,4 @@ module.exports = {
     'no-constant-condition': 'off',
     'no-control-regex': 'off'
   }
-}
+};
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,17 +2,17 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    target: 'node',
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        filename: 'bundle.js',
-        path: process.env.BUNDLE_JS_DIR ? path.resolve(process.env.BUNDLE_JS_DIR) : path.resolve(__dirname, 'dist'),
-        libraryTarget: 'commonjs2'
-    },
-    devtool: 'source-map',
-    optimization: {
-        minimize: false
-    }
+  target: 'node',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: process.env.BUNDLE_JS_DIR ? path.resolve(process.env.BUNDLE_JS_DIR) : path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: false
+  }
 };
